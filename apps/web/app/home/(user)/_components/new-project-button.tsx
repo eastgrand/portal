dacb4d068b4aa7e3ea879e5e46507413aea0a4d1
eastgrand <m.voldeck@gmail.com>
2025-01-08 17:29:05 -0500

    fix button

--- apps/web/app/home/(user)/_components/new-project-button.tsx
+++ apps/web/app/home/(user)/_components/new-project-button.tsx
@@ -2,13 +2,22 @@
 
 import { Button } from '@kit/ui/button';
 import { CreateProjectDialog } from '../../[account]/projects/_components/create-project-dialog';
+import { useCallback } from 'react';
 
 export function NewProjectButton() {
+  const handleClick = useCallback((e: React.MouseEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log('Button clicked'); // Debug log
+  }, []);
+
   return (
-    <CreateProjectDialog>
-      <Button type="button" onClick={(e) => e.preventDefault()}>
-        New Project
-      </Button>
-    </CreateProjectDialog>
+    <div onClick={handleClick}>
+      <CreateProjectDialog>
+        <Button type="button">
+          New Project
+        </Button>
+      </CreateProjectDialog>
+    </div>
   );
 }
