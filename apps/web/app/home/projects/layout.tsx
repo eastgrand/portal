d4eebe92f5074d62f23d975f8f8dcf79378c97da
eastgrand <m.voldeck@gmail.com>
2025-01-08 06:14:41 -0500

    fix projects list

--- apps/web/app/home/projects/layout.tsx
+++ apps/web/app/home/projects/layout.tsx
@@ -1,14 +1,32 @@
-export default function ProjectsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { use } from 'react';
+import { cookies } from 'next/headers';
+import { UserWorkspaceContextProvider } from '@kit/accounts/components';
+import {
+  Page,
+  PageLayoutStyle,
+  PageMobileNavigation,
+  PageNavigation,
+} from '@kit/ui/page';
+import { SidebarProvider } from '@kit/ui/shadcn-sidebar';
+import { Toaster } from '@kit/ui/toaster';
+import { AppLogo } from '~/components/app-logo';
+import { personalAccountNavigationConfig } from '~/config/personal-account-navigation.config';
+import { withI18n } from '~/lib/i18n/with-i18n';
+import { HomeMenuNavigation } from '../(user)/_components/home-menu-navigation';
+import { HomeMobileNavigation } from '../(user)/_components/home-mobile-navigation';
+import { HomeSidebar } from '../(user)/_components/home-sidebar';
+import { loadUserWorkspace } from '../(user)/_lib/server/load-user-workspace';
+
+async function ProjectsLayout({ children }: React.PropsWithChildren) {
+  // First, try just wrapping in basic layout
   return (
-    <>
-      {/* Test layout wrapper */}
-      <div className="flex min-h-screen w-full flex-col">
-        {children}
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col">
+      <div className="bg-red-100 p-4">Debug: Layout is rendering</div>
+      {children}
+    </div>
   );
 }
+
+export default withI18n(ProjectsLayout);
