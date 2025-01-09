b5ae58792c87096c732704ed6836f52db48c182f
eastgrand <m.voldeck@gmail.com>
2025-01-08 19:55:01 -0500

    fix button

--- apps/web/app/home/projects/page.tsx
+++ apps/web/app/home/projects/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { use } from 'react';
 import { headers } from 'next/headers';
