390e4aeb672f8973b9753ab1eefee8485a5d9c8f
eastgrand <m.voldeck@gmail.com>
2025-01-12 17:21:19 -0500

    layout updates

--- apps/web/app/home/projects/layout.tsx
+++ apps/web/app/home/projects/layout.tsx
@@ -35,9 +35,9 @@ function SidebarLayout({ children }: React.PropsWithChildren) {
               <HomeSidebar workspace={workspace} minimized={sidebarMinimized} />
             </aside>
             
-            <main className="flex-1 bg-gray-50 min-h-screen" 
+            <main className=" bg-gray-50 min-h-screen" 
                   style={{ 
-                    marginLeft: sidebarMinimized ? '80px' : '260px',
+                    marginLeft: sidebarMinimized ? '80px' : '256px',
                     marginTop: '64px'
                   }}>
               {children}
