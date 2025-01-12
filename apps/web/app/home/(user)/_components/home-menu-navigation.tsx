64ba800f5fe16af003fd4679c63c7b5fdf7484b0
eastgrand <m.voldeck@gmail.com>
2025-01-12 11:31:37 -0500

    layout updates

--- apps/web/app/home/(user)/_components/home-menu-navigation.tsx
+++ apps/web/app/home/(user)/_components/home-menu-navigation.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   BorderedNavigationMenu,
   BorderedNavigationMenuItem,
@@ -39,7 +40,6 @@ export function HomeMenuNavigation(props: { workspace: UserWorkspace }) {
   return (
     <div className={'flex w-full flex-1 justify-between'}>
       <div className={'flex items-center space-x-8'}>
-        <AppLogo />
 
         <BorderedNavigationMenu>
           {routes.map((route) => (
