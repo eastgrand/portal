9c088bb12392ca5de56c3ffa5a72c2991c0cfc02
eastgrand <m.voldeck@gmail.com>
2025-01-14 19:18:19 -0500

    account page

--- apps/web/app/home/[account]/layout.tsx
+++ apps/web/app/home/[account]/layout.tsx
@@ -1,21 +1,11 @@
 import { use } from 'react';
-
 import { cookies } from 'next/headers';
-
 import { TeamAccountWorkspaceContextProvider } from '@kit/team-accounts/components';
-import {
-  Page,
-  PageLayoutStyle,
-  PageMobileNavigation,
-  PageNavigation,
-} from '@kit/ui/page';
+import { Page, PageLayoutStyle } from '@kit/ui/page';
 import { SidebarProvider } from '@kit/ui/shadcn-sidebar';
-
 import { AppLogo } from '~/components/app-logo';
 import { getTeamAccountSidebarConfig } from '~/config/team-account-navigation.config';
 import { withI18n } from '~/lib/i18n/with-i18n';
-
-// local imports
 import { TeamAccountLayoutMobileNavigation } from './_components/team-account-layout-mobile-navigation';
 import { TeamAccountLayoutSidebar } from './_components/team-account-layout-sidebar';
 import { TeamAccountNavigationMenu } from './_components/team-account-navigation-menu';
@@ -28,12 +18,7 @@ type TeamWorkspaceLayoutProps = React.PropsWithChildren<{
 function TeamWorkspaceLayout({ children, params }: TeamWorkspaceLayoutProps) {
   const account = use(params).account;
   const style = use(getLayoutStyle(account));
-
-  if (style === 'sidebar') {
-    return <SidebarLayout account={account}>{children}</SidebarLayout>;
-  }
-
-  return <HeaderLayout account={account}>{children}</HeaderLayout>;
+  return style === 'sidebar' ? <SidebarLayout account={account}>{children}</SidebarLayout> : <HeaderLayout account={account}>{children}</HeaderLayout>;
 }
 
 function SidebarLayout({
@@ -43,42 +28,54 @@ function SidebarLayout({
   account: string;
 }>) {
   const data = use(loadTeamWorkspace(account));
-
   const accounts = data.accounts.map(({ name, slug, picture_url }) => ({
     label: name,
     value: slug,
     image: picture_url,
   }));
-
-  const minimized = getTeamAccountSidebarConfig(account).sidebarCollapsed;
+  const sidebarMinimized = getTeamAccountSidebarConfig(account).sidebarCollapsed;
 
   return (
     <TeamAccountWorkspaceContextProvider value={data}>
-      <SidebarProvider minimized={minimized}>
-        <Page style={'sidebar'}>
-          <PageNavigation>
-            <TeamAccountLayoutSidebar
-              account={account}
-              accountId={data.account.id}
-              accounts={accounts}
-              user={data.user}
-            />
-          </PageNavigation>
-
-          <PageMobileNavigation className={'flex items-center justify-between'}>
-            <AppLogo />
-
-            <div className={'flex space-x-4'}>
+      <SidebarProvider minimized={sidebarMinimized}>
+        <div className="min-h-screen">
+          <header className="h-16 fixed top-0 left-0 right-0 z-50 flex items-center px-4 bg-white border-b">
+            <div className="flex items-center flex-1">
+              <AppLogo />
+            </div>
+            <div className="flex items-center space-x-4">
+              <TeamAccountNavigationMenu workspace={data} />
               <TeamAccountLayoutMobileNavigation
                 userId={data.user.id}
                 accounts={accounts}
                 account={account}
               />
             </div>
-          </PageMobileNavigation>
+          </header>
 
-          {children}
-        </Page>
+          <div className="flex w-full">
+            <aside className="fixed top-16 h-[calc(100vh-4rem)] bg-white border-r">
+              <TeamAccountLayoutSidebar
+                account={account}
+                accountId={data.account.id}
+                accounts={accounts}
+                user={data.user}
+              />
+            </aside>
+            
+            <main 
+              className="bg-gray-50 min-h-screen flex-1 w-[calc(100vw-256px)]" 
+              style={{ 
+                marginLeft: sidebarMinimized ? '80px' : '256px',
+                marginTop: '64px'
+              }}
+            >
+              <div className="w-full h-full">
+                {children}
+              </div>
+            </main>
+          </div>
+        </div>
       </SidebarProvider>
     </TeamAccountWorkspaceContextProvider>
   );
@@ -91,7 +88,6 @@ function HeaderLayout({
   account: string;
 }>) {
   const data = use(loadTeamWorkspace(account));
-
   const accounts = data.accounts.map(({ name, slug, picture_url }) => ({
     label: name,
     value: slug,
@@ -101,23 +97,23 @@ function HeaderLayout({
   return (
     <TeamAccountWorkspaceContextProvider value={data}>
       <Page style={'header'}>
-        <PageNavigation>
-          <TeamAccountNavigationMenu workspace={data} />
-        </PageNavigation>
-
-        <PageMobileNavigation className={'flex items-center justify-between'}>
-          <AppLogo />
-
-          <div className={'group-data-[mobile:hidden]'}>
+        <div className="h-16 flex items-center px-4 bg-white border-b">
+          <div className="flex items-center flex-1">
+            <AppLogo />
+          </div>
+          <div className="flex items-center space-x-4">
+            <TeamAccountNavigationMenu workspace={data} />
             <TeamAccountLayoutMobileNavigation
               userId={data.user.id}
               accounts={accounts}
               account={account}
             />
           </div>
-        </PageMobileNavigation>
+        </div>
 
-        {children}
+        <div className="flex-1 bg-gray-50">
+          {children}
+        </div>
       </Page>
     </TeamAccountWorkspaceContextProvider>
   );
@@ -125,7 +121,6 @@ function HeaderLayout({
 
 async function getLayoutStyle(account: string) {
   const cookieStore = await cookies();
-
   return (
     (cookieStore.get('layout-style')?.value as PageLayoutStyle) ??
     getTeamAccountSidebarConfig(account).style
