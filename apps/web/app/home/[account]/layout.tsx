48a8d8a91511693771489b85c55278b01e1f3c6b
eastgrand <m.voldeck@gmail.com>
2025-01-17 20:13:03 -0500

    hide sidebar dropdown

--- apps/web/app/home/[account]/layout.tsx
+++ apps/web/app/home/[account]/layout.tsx
@@ -44,18 +44,12 @@ function SidebarLayout({
 
           <div className="flex w-full">
             <aside className="fixed top-16 h-[calc(100vh-4rem)] bg-white border-r">
-              <div className="hidden">
-                <TeamAccountLayoutSidebar
-                  account={account}
-                  accountId={data.account.id}
-                  accounts={data.accounts.map(({ name, slug, picture_url }) => ({
-                    label: name,
-                    value: slug,
-                    image: picture_url
-                  }))}
-                  user={data.user}
-                />
-              </div>
+              <TeamAccountLayoutSidebar
+                account={account}
+                accountId={data.account.id}
+                accounts={[]} // Pass empty array to hide the dropdown
+                user={data.user}
+              />
             </aside>
             
             <main 
